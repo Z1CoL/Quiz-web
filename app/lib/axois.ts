2cc7a64efb90a372af3e5cb2a115e01d7fdff9aa
ZiCoL <zcol4615@gmail.com>
2026-02-27 15:54:05 +0800

    added

--- app/lib/axois.ts
+++ app/lib/axois.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "https://quiz-web-omega-two.vercel.app/", // эсвэл deployed domain
-});
+// const axiosInstance = axios.create({
+//   baseURL: "https://quiz-web-omega-two.vercel.app/", // эсвэл deployed domain
+// });
 
+
+const axiosInstance = axios.create();
 export default axiosInstance;
