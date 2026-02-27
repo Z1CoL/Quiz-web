741625565dde4549132e9189488f7b34e7703f8e
ZiCoL <zcol4615@gmail.com>
2026-02-27 16:18:28 +0800

    added

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+});
 
 export default axiosInstance;
