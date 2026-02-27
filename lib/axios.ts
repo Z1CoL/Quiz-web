6cd1c0880002b51dfcfab0e2d37d075ec03d3605
ZiCoL <zcol4615@gmail.com>
2026-02-27 15:46:38 +0800

    added

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://quiz-web-omega-two.vercel.app/", 
+    baseURL: "https://quiz-web-omega-two.vercel.app", 
 });
 
 export default axiosInstance;
